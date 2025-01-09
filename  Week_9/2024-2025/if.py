1d1d4d5ad3a47ab68484d797c5f19b8ad0aaed82
Yonatan Bernal Mendez <ybernalmendez@bostonk12.org>
2025-01-09 10:33:42 -0500

    tarea de hoy 25/9/1

---
+++  Week_9/2024-2025/if.py
@@ -0,0 +1,9 @@
+#yonatan
+#20205-01-09
+
+num1=int(input("pealse enter a number:"))
+num2=int(input("pealse another number:"))
+if num1>num2:
+    print("The biggest number that I have seen so far is ",num1)
+else:
+    print("The smallest number I have seen so far is ",num2)
