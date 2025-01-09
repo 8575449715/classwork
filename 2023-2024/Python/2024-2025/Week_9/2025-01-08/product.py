2a40cd2649d29babaaf56e217d89d09d7604c2a8
Yonatan Bernal Mendez <ybernalmendez@bostonk12.org>
2025-01-09 09:26:59 -0500

    Enter a number20 Entern a other number30 Enter a third number50

---
+++ 2023-2024/Python/2024-2025/Week_9/2025-01-08/product.py
@@ -0,0 +1,10 @@
+#yonatan
+#2025-5-1
+
+num1=int(input('enter a number"))
+nu2=int(input("enter another number"))
+num3=int(input('enter a third number'))
+
+product=num1*num2*num3
+print(product)
+print(num1,"x",num2,"x",num3,"=",product)
