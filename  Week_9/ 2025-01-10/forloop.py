65c63b479f2dd6cd5039b48f0af0c1a10520ea25
Yonatan Bernal Mendez <ybernalmendez@bostonk12.org>
2025-03-14 09:00:54 -0400

    tarea de hoy 25/9/1

---
+++  Week_9/ 2025-01-10/forloop.py
@@ -0,0 +1,12 @@
+#YONATAN
+#2025-01-10
+
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+
+age=input("Enter your age: ")
+print()
+print("hello", first_name, last_name)
+print("you are 15 years old sou yo should be about this tall")
+for i in range(15):
+    print({"*"})
