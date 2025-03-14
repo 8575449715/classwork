3b936f7308d4a1461c7d6ab62a7994e51834cd34
Yonatan Bernal Mendez <ybernalmendez@bostonk12.org>
2025-03-14 09:05:07 -0400

    tarea de hoy 25/9/1

---
+++ Week_8/2024-12-12/variables.py
@@ -0,0 +1,16 @@
+# yonatan
+# 2024-12-12
+
+# input, variables and types
+
+name = input('enter your name: yonatan')
+print (type(name))
+#it was a string
+
+age =input("enter your age;")
+print(type(age))
+#it was a string
+
+age=int(age)
+print(type(age))
+# int
